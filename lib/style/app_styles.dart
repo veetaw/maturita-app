d0ce657da8774e7a70d8d10c587f9572bfc695ea
vito <vitopiegari01@gmail.com>
2020-06-01 22:45:24 +0200

    App theme

---
+++ lib/style/app_styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppStyles {
+  static const Color kPrimaryColor = Color(0xff3A5A40);
+  static const Color kAccentColor = Color(0xff3A5A40);
+  static const Color kSecondaryColor = Color(0xff588157);
+  static const Color kSecondaryAccentColor = Color(0xffA3B18A);
+  static const Color kBackgroundColor = Color(0xffDAD7CD);
+  static const Color kCardColor = Color(0xffEEECE8);
+}
+
+const double kStandardPadding = 32;
