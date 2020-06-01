e5ee63078fc596a379a251af6be1daed882be5a0
vito <vitopiegari01@gmail.com>
2020-06-01 22:44:42 +0200

    Added constants for storage

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,7 @@ const bool kIsDebug = true;
 
 // TODO: prod API url
 const String kApiBaseUrl = kIsDebug ? 'http://localhost:3000/api' : '';
+
+// persist_login.dart
+const String kTokenKey = 'token';
+const String kUserTypeKey = 'type';
