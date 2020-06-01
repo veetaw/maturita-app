d28ff12a404f99e59e0152db90f01c470bdb7ab5
vito <vitopiegari01@gmail.com>
2020-06-01 22:45:56 +0200

    Implemented owner registration

---
+++ lib/screens/auth/register_owner.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+import 'package:pizza/common/widget/circle_selector_avatar.dart';
+import 'package:pizza/common/widget/custom_input_text.dart';
+import 'package:pizza/screens/auth/common.dart';
+import 'package:pizza/screens/auth/create_pizzeria.dart';
+import 'package:pizza/style/app_styles.dart';
+
+class RegisterOwner extends StatelessWidget {
+  static const String kRouteName = 'login/registerOwner';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppStyles.kBackgroundColor,
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            if (constraints.maxHeight > constraints.maxWidth)
+              return _buildPortrait(context, constraints);
+            else
+              return _buildLandscape(context, constraints);
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPortrait(BuildContext context, BoxConstraints constraints) {
+    return SingleChildScrollView(
+      physics: BouncingScrollPhysics(),
+      child: Container(
+        width: constraints.maxWidth,
+        height: constraints.maxHeight,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: kStandardPadding),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: _buildTitle(),
+                    ),
+                    CircleSelectorAvatar(onTap: () {}),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 6,
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: kStandardPadding),
+                child: _buildFields(),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: kStandardPadding),
+                child: _buildButton(context),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Column _buildButton(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        buildNextButton(
+          () {
+            Navigator.of(context).pushNamed(
+              CreatePizzeria.kRouteName,
+            );
+          },
+          "Avanti",
+        ),
+      ],
+    );
+  }
+
+  Form _buildFields() {
+    return Form(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          CustomInputText(
+            labelText: "First name",
+            prefixIcon: Icon(
+              Icons.person,
+              color: AppStyles.kPrimaryColor,
+            ),
+          ),
+          CustomInputText(
+            labelText: "Last name",
+            prefixIcon: Icon(
+              Icons.person,
+              color: AppStyles.kPrimaryColor,
+            ),
+          ),
+          CustomInputText(
+            labelText: "email",
+            prefixIcon: Icon(
+              Icons.email,
+              color: AppStyles.kPrimaryColor,
+            ),
+          ),
+          PasswordInputText(),
+        ],
+      ),
+    );
+  }
+
+  List<Widget> _buildTitle() {
+    return [
+      Text(
+        "Crea",
+        style: TextStyle(
+          fontSize: 48,
+          fontWeight: FontWeight.w200,
+        ),
+      ),
+      Text(
+        'Account',
+        style: TextStyle(
+          fontSize: 48,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    ];
+  }
+
+  Widget _buildLandscape(BuildContext context, BoxConstraints constraints) {
+    return SingleChildScrollView(
+      physics: BouncingScrollPhysics(),
+      child: Container(
+        width: constraints.maxWidth,
+        height: constraints.maxHeight,
+        child: Row(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: kStandardPadding),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: buildTitle('Crea', 'Account'),
+                        ),
+                        CircleSelectorAvatar(onTap: () {}),
+                      ],
+                    ),
+                    _buildButton(context),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: _buildFields(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
