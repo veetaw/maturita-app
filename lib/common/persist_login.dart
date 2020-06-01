676456bab88ec45d676b0ea12b994046af555bea
vito <vitopiegari01@gmail.com>
2020-06-01 22:44:58 +0200

    Persistent secure storage utils

---
+++ lib/common/persist_login.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:pizza/constants.dart';
+
+final FlutterSecureStorage storage = FlutterSecureStorage();
+
+Future<String> getToken() async => storage.read(key: kTokenKey);
+
+Future saveToken(String token) async =>
+    storage.write(key: kTokenKey, value: token);
+
+Future<bool> isLoggedIn() async {
+  String token = await getToken();
+  return token != null && token.isNotEmpty;
+}
+
+Future saveUserType(String type) async =>
+    storage.write(key: kUserTypeKey, value: type);
+
+Future<bool> isUser() async {
+  String type = await storage.read(key: kUserTypeKey);
+  return type != null && type.isNotEmpty && type.toLowerCase() == 'user';
+}
