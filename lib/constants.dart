95d31fb797344e6f7744bdb769a4b0191b48ae19
vito <vitopiegari01@gmail.com>
2020-06-02 14:45:34 +0200

    Update server address

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 const bool kIsDebug = true;
 
 // TODO: prod API url
-const String kApiBaseUrl = kIsDebug ? 'http://localhost:3000/api' : '';
+const String kApiBaseUrl = kIsDebug ? 'http://192.168.1.245:3000/api' : '';
 
 // persist_login.dart
 const String kTokenKey = 'token';
