776f221fae428d7c8ff0a6fb5a0a273973f6b8fb
vito <vitopiegari01@gmail.com>
2020-06-02 14:46:22 +0200

    Implementation of login

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:pizza/common/persist_login_abstract.dart';
+import 'package:pizza/common/validate_email.dart';
 import 'package:pizza/common/widget/custom_input_text.dart';
 import 'package:pizza/common/widget/custom_switch.dart';
 import 'package:pizza/screens/auth/common.dart';
 import 'package:pizza/screens/auth/register_owner.dart';
 import 'package:pizza/screens/auth/register_user.dart';
+import 'package:pizza/screens/owner/owner_home.dart';
+import 'package:pizza/screens/user/user_home.dart';
+import 'package:pizza/services/owner_api.dart';
+import 'package:pizza/services/user_api.dart';
 import 'package:pizza/style/app_styles.dart';
 import 'package:provider/provider.dart';
+
+final GlobalKey<FormState> formKey = GlobalKey();
+final TextEditingController emailController = TextEditingController();
+final TextEditingController passwordController = TextEditingController();
 
 class Login extends StatelessWidget {
   static const String kRouteName = 'login';
@@ -18,13 +28,17 @@ class Login extends StatelessWidget {
       body: SafeArea(
         child: ChangeNotifierProvider<SwitchNotifier>(
           create: (_) => SwitchNotifier(),
-          child: LayoutBuilder(
-            builder: (context, constraints) {
-              if (constraints.maxHeight > constraints.maxWidth)
-                return _buildPortrait(context, constraints);
-              else
-                return _buildLandscape(context, constraints);
-            },
+          child: Form(
+            key: formKey,
+            autovalidate: true,
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                if (constraints.maxHeight > constraints.maxWidth)
+                  return _buildPortrait(context, constraints);
+                else
+                  return _buildLandscape(context, constraints);
+              },
+            ),
           ),
         ),
       ),
@@ -88,6 +102,10 @@ class Login extends StatelessWidget {
   }
 
   List<Widget> _buildButtons(BuildContext context, BoxConstraints constraints) {
+    int activeIndex = Provider.of<SwitchNotifier>(
+      context,
+      listen: false,
+    ).activeIndex;
     return [
       CustomSwitch(
         constraints: constraints,
@@ -95,29 +113,68 @@ class Login extends StatelessWidget {
             Provider.of<SwitchNotifier>(context, listen: false).activeIndex = i,
       ),
       buildNextButton(() {
-        if (Provider.of<SwitchNotifier>(
-              context,
-              listen: false,
-            ).activeIndex ==
-            0)
+        if (activeIndex == 0)
           Navigator.of(context).pushNamed(RegisterUser.kRouteName);
         else
           Navigator.of(context).pushNamed(RegisterOwner.kRouteName);
       }, "Registrati"),
-      buildNextButton(() {}, "Entra"),
+      buildNextButton(() {
+        error(_) {
+          Scaffold.of(context).showSnackBar(
+            SnackBar(
+              content: Text("Login non riuscito"),
+            ),
+          );
+        }
+
+        success(String token) {
+          PersistLogin().saveToken(token);
+          PersistLogin().saveUserType(activeIndex == 0 ? 'user' : 'owner');
+
+          Navigator.of(context).pushNamedAndRemoveUntil(
+            activeIndex == 0 ? UserHome.kRouteName : OwnerHome.kRouteName,
+            (route) => false,
+          );
+        }
+
+        if (formKey.currentState.validate()) {
+          if (activeIndex == 0) {
+            UserApi()
+                .login(
+                  email: emailController.text,
+                  password: passwordController.text,
+                )
+                .then(success)
+                .catchError(error);
+          } else {
+            OwnerApi()
+                .login(
+                  email: emailController.text,
+                  password: passwordController.text,
+                )
+                .then(success)
+                .catchError(error);
+          }
+        }
+      }, "Entra"),
     ];
   }
 
   List<Widget> _buildInputTexts() {
     return [
       CustomInputText(
+        controller: emailController,
         labelText: "Email",
+        keyboardType: TextInputType.emailAddress,
+        validator: validateEmail,
         prefixIcon: Icon(
           Icons.person,
           color: AppStyles.kPrimaryColor,
         ),
       ),
-      PasswordInputText(),
+      PasswordInputText(
+        controller: passwordController,
+      ),
     ];
   }
 
